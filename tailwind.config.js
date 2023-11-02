d9df8ab0eb1f3da6a102fe3e284b02a3689157d7
sio-salt <saltinthedesertyou@gmail.com>
2023-11-02 13:59:22 +0900

    add before first deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        // './resources/views/**/*.blade.php',
+        './resources/views/**/*.blade.php',
     ],
 
     theme: {
@@ -13,49 +13,8 @@ module.exports = {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
-            colors: {
-                brown: {
-                    50: '#fdf8f6',
-                    100: '#f2e8e5',
-                    200: '#eaddd7',
-                    300: '#e0cec7',
-                    400: '#d2bab0',
-                    500: '#bfa094',
-                    600: '#a18072',
-                    700: '#977669',
-                    800: '#846358',
-                    900: '#43302b',
-                },
-                blue: {
-                    50: '#eff6ff',
-                    100: '#dbeafe',
-                    200: '#bfdbfe',
-                    300: '#93c5fd',
-                    400: '#60a5fa',
-                    500: '#3b82f6',
-                    600: '#2563eb',
-                    700: '#1d4ed8',
-                    800: '#1e40af',
-                    900: '#1e3a8a',
-                },
-                green: {
-                    50: '#0A5137',
-                    100: '#0E6445',
-                    200: '#137752',
-                    300: '#18895F',
-                    400: '#1D9A6C',
-                    // 500: '#3FAC84',
-                    500: '#2cb044',
-                    600: '#62BE9C',
-                    700: '#86CEB4',
-                    800: '#AADECB',
-                    900: '#CFEDE2',
-                    DEFAULT: '#3FAC84',
-                },
-            }
         },
     },
 
     plugins: [require('@tailwindcss/forms')],
 };
-
