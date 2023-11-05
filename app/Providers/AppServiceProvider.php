09d7743ac7d2a336c4ecf9e0b75fee8c0cf7a29d
sio-salt <saltinthedesertyou@gmail.com>
2023-11-05 12:23:56 +0900

    Add  varchar = 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,5 +30,6 @@ class AppServiceProvider extends ServiceProvider
         \URL::forceScheme('https');
         $this->app['request']->server->set('HTTPS', 'on');
         // Paginator::defaultView('my-pagination.blade.php');
+        Schema::defaultStringLength(191);
     }
 }
