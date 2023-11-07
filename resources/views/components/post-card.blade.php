f9820fecfe827ce84f13b0be1e2500d3aa26adb4
sio-salt <saltinthedesertyou@gmail.com>
2023-11-07 17:03:51 +0900

    remove help page and dark mode

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -12,8 +12,7 @@
             
                     <div x-data="{ isOpen: false }" class="relative inline-block">
                         <!-- Dropdown toggle button -->
-                        <!--<button @click="isOpen = !isOpen" class="relative z-10 block p-2 text-gray-700 bg-white border border-transparent rounded-md dark:text-white focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:ring dark:bg-gray-800 focus:outline-none">-->
-                        <button @click="isOpen = !isOpen" class="relative block p-2 text-gray-700 bg-white border border-transparent rounded-md dark:text-white focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:ring dark:bg-gray-800 focus:outline-none">
+                        <button @click="isOpen = !isOpen" class="relative block p-2 text-gray-700 bg-white border border-transparent rounded-md focus:border-blue-500 focus:ring-opacity-40 focus:ring-blue-300 focus:ring focus:outline-none">
                             <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
                             </svg>
@@ -28,14 +27,13 @@
                             x-transition:leave="transition ease-in duration-100"
                             x-transition:leave-start="opacity-100 scale-100"
                             x-transition:leave-end="opacity-0 scale-90" 
-                            {{--class="absolute right-0 z-20 w-20 py-2 mt-2 origin-top-right bg-white rounded-md shadow-xl dark:bg-gray-800"--}}
-                            class="absolute right-0 w-20 py-2 mt-2 origin-top-right bg-white rounded-md shadow-xl dark:bg-gray-800"
+                            class="absolute right-0 w-20 py-2 mt-2 origin-top-right bg-white rounded-md shadow-xl"
                         >
-                            <a href="{{ route('edit_post', $post) }}" class="block px-4 py-1 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"> {{ __('Edit') }} </a>
+                            <a href="{{ route('edit_post', $post) }}" class="block px-4 py-1 text-sm text-gray-600 capitalize transition-colors duration-300 transform hover:bg-gray-100"> {{ __('Edit') }} </a>
                             <form action="{{ route('delete_post', $post) }}" id="form_{{ $post->id }}" method="post" class="">
                                 @csrf
                                 @method('DELETE')
-                                <button type="button" onclick="deletePost({{ $post->id }})" class="w-full text-left block px-4 py-1 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">{{ __('Delete') }}</button>
+                                <button type="button" onclick="deletePost({{ $post->id }})" class="w-full text-left block px-4 py-1 text-sm text-gray-600 capitalize transition-colors duration-300 transform hover:bg-gray-100">{{ __('Delete') }}</button>
                             </form>
                         </div>
                     </div>        
