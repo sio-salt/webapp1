6dbfbd46b6a67e210ec6889aa94e91f64ca4d6bd
sio-salt <saltinthedesertyou@gmail.com>
2024-01-09 10:16:26 +0900

    Add guest functionality to participate button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/participate.js'])
+        {{--@vite(['resources/css/app.css', 'resources/js/app.js'])--}}
         
     </head>
     <body class="font-sans antialiased">
